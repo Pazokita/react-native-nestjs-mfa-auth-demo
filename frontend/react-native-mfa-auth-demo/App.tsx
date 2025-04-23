dc2feba067a6660f90ad9990d64bc013f424f7a2
Pazokita <amalia.maturana@gmail.com>
2025-04-23 19:03:19 +0200

    refactor(app): replace LoginScreen with AppNavigator for conditional routing

--- frontend/react-native-mfa-auth-demo/App.tsx
+++ frontend/react-native-mfa-auth-demo/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useFonts } from 'expo-font';
 import { Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 import { AuthProvider } from './src/features/auth/context/AuthProvider';
-import LoginScreen from './src/features/auth/screens/LoginScreen';
+import AppNavigator from './src/shared/navigation/AppNavigator';
 
 
 export default function App() {
@@ -14,7 +14,7 @@ export default function App() {
   if (!fontsLoaded) return null;
   return (
     <AuthProvider>
-      <LoginScreen />
+      <AppNavigator />
     </AuthProvider>
   );
 }
