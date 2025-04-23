9236d872be71fbd0035e3b25576aceb59de23bac
Pazokita <amalia.maturana@gmail.com>
2025-04-23 18:56:53 +0200

    chore(api): connect frontend to backend with axios

--- frontend/react-native-mfa-auth-demo/src/shared/utils/axiosInstance.ts
+++ frontend/react-native-mfa-auth-demo/src/shared/utils/axiosInstance.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 import Constants from 'expo-constants';
 
 const API_URL = Constants.expoConfig?.extra?.apiUrl || 'http://localhost:3000';
-
 const axiosInstance = axios.create({
   baseURL: API_URL,
   headers: {
